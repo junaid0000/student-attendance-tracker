220a489e4eb29213643b766e19e63b370e3ef71d
Junaid <64260129+junaid0000@users.noreply.github.com>
2025-12-02 00:08:24 +0100

    Add test for student ID auto-generation

--- src/test/java/attendance/tracker/model/testCreateStudentWithAutoId.java
+++ src/test/java/attendance/tracker/model/testCreateStudentWithAutoId.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class testCreateStudentWithAutoId {
-	//Test auto student id generate.
 	
 	@Test
 	public void testCreateStudentWithAutoId() {
