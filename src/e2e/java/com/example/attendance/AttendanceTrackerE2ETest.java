cc136775eb0536672ec7624014a4a3b77990ceab
Junaid <64260129+junaid0000@users.noreply.github.com>
2026-01-28 16:14:12 +0100

    Add MongoDB Testcontainer as ClassRule

--- src/e2e/java/com/example/attendance/AttendanceTrackerE2ETest.java
+++ src/e2e/java/com/example/attendance/AttendanceTrackerE2ETest.java
@@ -2,15 +2,20 @@ package com.example.attendance;
 
 import org.assertj.swing.junit.runner.GUITestRunner;
 import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
+import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.testcontainers.containers.MongoDBContainer;
 
 @RunWith(GUITestRunner.class)
 public class AttendanceTrackerE2ETest extends AssertJSwingJUnitTestCase {
 
+    @ClassRule
+    public static final MongoDBContainer mongo = new MongoDBContainer("mongo:4.4.3");
+
     @Test
     public void testApplicationStartsSuccessfully() {
-        // This will fail not i implement this E2E setup yet
-        throw new AssertionError("Not implemented yet");
+        // so it will fail 
+        throw new AssertionError("startup not implemented ");
     }
 }
