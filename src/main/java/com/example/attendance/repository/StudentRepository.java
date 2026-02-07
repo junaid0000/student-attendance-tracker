05aa6dd5426d2e9306a6d1744e8ac1b8a78e5e10
Junaid <64260129+junaid0000@users.noreply.github.com>
2026-02-07 16:21:27 +0100

    fixig

--- src/main/java/com/example/attendance/repository/StudentRepository.java
+++ src/main/java/com/example/attendance/repository/StudentRepository.java
@@ -1,5 +1,6 @@
 package com.example.attendance.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import com.example.attendance.model.Student;
@@ -8,4 +9,6 @@ public interface StudentRepository {
     Student save(Student student);
     Optional<Student> findByRollNumber(String rollNumber);
     void delete(Student student); // for delete
+    List<Student> findAll();
+    void update(Student student);
 }
