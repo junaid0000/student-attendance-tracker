eef378a059321f9873f52b593a06464da7cb0c6c
Junaid <64260129+junaid0000@users.noreply.github.com>
2026-04-02 23:42:17 +0200

    fixing

--- src/test/java/com/example/attendance/controller/StudentControllerTest.java
+++ src/test/java/com/example/attendance/controller/StudentControllerTest.java
@@ -42,7 +42,6 @@ public class StudentControllerTest {
     public void testAddStudent() {
         Student student = new Student("Junaid", "7131056");
         when(studentRepository.save(any(Student.class))).thenReturn(student);
-
         Student result = studentController.addStudent("Junaid", "7131056");
 
         assertNotNull(result);
@@ -54,14 +53,13 @@ public class StudentControllerTest {
     public void testUpdateStudent() {
         Student student = new Student("Junaid", "7131056");
         when(studentRepository.findByRollNumber("7131056")).thenReturn(Optional.of(student));
-        when(studentRepository.save(any(Student.class))).thenReturn(student);
-
         Student result = studentController.updateStudent("7131056", "Junaid Updated", "7131056");
 
         assertNotNull(result);
         verify(studentRepository).findByRollNumber("7131056");
-        verify(studentRepository).save(any(Student.class));
+        verify(studentRepository).update(any(Student.class));
     }
+
 
     // Delete student
     @Test
